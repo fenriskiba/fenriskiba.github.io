78560e7c20b4308b2db181ad83b7877cfff52a06
Grant Carroll <gcarroll2603@gmail.com>
2024-03-15 16:29:34 -0400

    updated aliases

--- scripts/desktop-zsh-profile.sh
+++ scripts/desktop-zsh-profile.sh
@@ -16,16 +16,28 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# Random Alias
-alias tri-mon-dup="xrandr --output DP-2 -s 3840x2160 --rate 120 --output DP-4 --same-as DP-2 --output HDMI-0 --same-as DP-2"
+# Git Aliases
+alias gitk="git fetch --all && git remote prune origin && gitk --all --date-order --full-history&"
+alias git-code='GIT_EDITOR=code git'
 
-alias gitk="gitk --all --date-order&"
-alias chrome="google-chrome"
-
-alias zsh-profile="code ~/.zshrc"
-
+# Podman Aliases
 alias podman-cleanup="podman system prune --all --force && podman rmi --all && podman volume rm --all"
 alias docker="podman"
 # alias docker-compose="podman-compose"
+
+# Misc Aliases
+alias zsh-profile="code ~/.zshrc"
+alias make-script-executable="chmod +x"
+alias tri-mon-dup="xrandr --output DP-2 -s 3840x2160 --rate 120 --output DP-4 --same-as DP-2 --output HDMI-0 --same-as DP-2"
+
+# Echo Proxy Variables
+proxy-vars() {
+    echo "HTTPS_PROXY:" $HTTPS_PROXY
+    echo "https_proxy:" $https_proxy
+    echo "HTTP_PROXY: " $HTTP_PROXY
+    echo "http_proxy: " $http_proxy
+    echo "NO_PROXY:   " $NO_PROXY
+    echo "no_proxy:   " $no_proxy
+}
 
 cp ~/.zshrc ~/projects/fenriskiba.github.io/scripts/desktop-zsh-profile.sh
