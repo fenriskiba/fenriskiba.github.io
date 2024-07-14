9a6b58d9f5dff2fd7c0e197cc009a77219561359
Grant I Carroll <gcarroll2603@gmail.com>
2024-07-14 16:01:02 -0400

    updated monitor sync script

--- scripts/monitor-sync.sh
+++ scripts/monitor-sync.sh
@@ -15,32 +15,9 @@ dbus-monitor --session "type=signal,interface=org.cinnamon.ScreenSaver,member=Ac
     LOCK_STAT=`echo $MSG | awk '{print $NF}'`
     if [[ "$LOCK_STAT" == "member=ActiveChanged" ]]; then
         echo "was unlocked"
-        #!/bin/bash
-
-CAUGHT_SHUTDOWN="0"
-trap handleShutdown SIGTERM
-
-function handleShutdown()
-{
-    echo "caught shutdown SIGTERM signal"
-    CAUGHT_SHUTDOWN="1"
-    exit 0
-}
-
-dbus-monitor --session "type=signal,interface=org.cinnamon.ScreenSaver,member=ActiveChanged" | 
-  while read MSG; do
-    LOCK_STAT=`echo $MSG | awk '{print $NF}'`
-    if [[ "$LOCK_STAT" == "member=ActiveChanged" ]]; then
-        echo "was unlocked"
         xrandr --output DP-4 -s 3840x2160 --rate 120 \
                --output DP-2 --same-as DP-4 \
                --output HDMI-0 --same-as DP-4
-    fi
-
-    if [ $CAUGHT_SHUTDOWN != "0" ]; then
-        break
-    fi
-  done
     fi
 
     if [ $CAUGHT_SHUTDOWN != "0" ]; then
